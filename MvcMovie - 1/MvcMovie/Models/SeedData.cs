968f3c14b22c00e54efdb7448b82a7edf3715a4d
Daniel Santos "Frank Deckard" Lopes Silva <71890228+MrCyberpunKx@users.noreply.github.com>
2021-03-05 11:36:33 -0300

    Add files via upload
    I used MVC in Visual Studio Community 2019, to create an application, about movies that I like, they are just examples to show the functionality of the project.
    As support for project creation, I used the Microsoft Docs Tutorial: Aspnet Core Tutorials First-mvc-app.

---
+++ MvcMovie - 1/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Blade Runner",
+                        ReleaseDate = DateTime.Parse("25-6-1982"),
+                        Genre = "SciFi/Ação/Fantasia",
+                        Price = 28M,
+                        Rating = "10"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Blade Runner 2049 ",
+                        ReleaseDate = DateTime.Parse("3-10-2017"),
+                        Genre = "SciFi/Thriller",
+                        Price = 185M,
+                        Rating = "10"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rambo",
+                        ReleaseDate = DateTime.Parse("22-10-1982"),
+                        Genre = "Ação/Drama",
+                        Price = 15M,
+                        Rating = "10"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Escola Do Rock",
+                        ReleaseDate = DateTime.Parse("3-10-2003"),
+                        Genre = "Comédia",
+                        Price = 35M,
+                        Rating = "9"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
